1b7cc619d31d5f73251144f88106fe89d90575e7
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-12-18 23:27:28 +0545

    added space

--- lib/screen/dashboard/widgets/quick_access.dart
+++ lib/screen/dashboard/widgets/quick_access.dart
@@ -10,10 +10,12 @@ class QuickAccess extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Text("Quick Access", style: TextStyle(color: Colors.white70)),
-        const SizedBox(height: 12),
+        const SizedBox(height: 10),
         GridView.count(
           crossAxisCount: 2,
           shrinkWrap: true,
+          crossAxisSpacing: 5, // horizontal spacing
+          mainAxisSpacing: 5, // vertical spacing
           physics: const NeverScrollableScrollPhysics(),
           children: [
             IconBox(icon: Icons.description, color: Colors.orange),
