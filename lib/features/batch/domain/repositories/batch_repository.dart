53f33a3fd2ff8643d7d4fd4979f84efb09a157aa
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2026-01-02 23:19:33 +0545

    unwanted file

--- lib/features/batch/domain/repositories/batch_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_application_1/features/batch/domain/entites/batch_entity.dart';
-
-abstract class BatchRepository {
-  Future<Either<Failure, List<BatchEntity>>> getAllBatches();
-  Future<Either<Failure, BatchEntity>> getBatchById(String batchId);
-  Future<Either<Failure, bool>> createBatch(BatchEntity entity);
-  Future<Either<Failure, bool>> updateBatch(BatchEntity entity);
-  Future<Either<Failure, bool>> deleteBatch(String batchID);
-}
